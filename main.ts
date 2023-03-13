90b3011c7b886816f319286f46aea556828c83fd
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-03-13 11:04:54 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-let strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+radio.onReceivedNumber(function (receivedNumber) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+})
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+radio.setGroup(35)
 basic.forever(function () {
-    strip.showColor(neopixel.rgb(255, 255, 255))
+    strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+    strip.show()
 })
