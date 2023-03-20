a8b8b57f348be90cc0fe051545a71ba9172594c1
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-03-20 11:12:21 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ radio.onReceivedNumber(function (receivedNumber) {
 	
 })
 input.onButtonPressed(Button.A, function () {
+    bucle = bucle
     for (let index = 0; index < 25500; index++) {
         color += 1
         strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
@@ -14,16 +15,19 @@ input.onButtonPressed(Button.A, function () {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 })
 input.onButtonPressed(Button.B, function () {
+    bucle = 0
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     strip.showColor(neopixel.rgb(0, 0, 0))
     strip.show()
 })
+let bucle = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 radio.setGroup(35)
 let color = 0
+bucle = 0
 basic.forever(function () {
 	
 })
