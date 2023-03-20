581fa42c0bd57da75bb8b7fbb717ffb9e3e0f26b
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-03-20 11:09:25 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ radio.onReceivedNumber(function (receivedNumber) {
 	
 })
 input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 25500; index++) {
+        color += 1
+        strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+        strip.show()
+        basic.pause(200)
+    }
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
@@ -11,15 +17,13 @@ input.onButtonPressed(Button.B, function () {
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    strip.showColor(neopixel.rgb(0, 0, 0))
+    strip.show()
 })
-let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 radio.setGroup(35)
 let color = 0
 basic.forever(function () {
-    for (let index = 0; index < 255; index++) {
-        color += 1
-        strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-        strip.show()
-        basic.pause(200)
-    }
+	
 })
