26002a32a754a9777069a0701a498049fc8edad6
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-03-27 11:02:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,26 +7,25 @@ radio.onReceivedString(function (receivedString) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        strip.showColor(neopixel.rgb(255, 0, 0))
+        strip.show()
     } else {
         if (receivedString == "Adios") {
             bucle = 0
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
             maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            strip.showColor(neopixel.rgb(0, 0, 0))
+            strip.show()
         }
     }
 })
 let bucle = 0
-let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 radio.setGroup(35)
 let color = 0
 bucle = 0
 basic.forever(function () {
-    bucle = bucle
-    for (let index = 0; index < 25500; index++) {
-        color += 1
-        strip.showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-        strip.show()
-        basic.pause(200)
-    }
+	
 })
