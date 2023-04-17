ba97c8b187a8368170c1a22bbaafb1926a99282e
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-04-17 11:10:01 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("parar")
-    strip.showColor(neopixel.rgb(0, 255, 0))
+    strip.showColor(neopixel.rgb(255, 236, 0))
     strip.show()
     maqueen.motorStop(maqueen.Motors.All)
     basic.showString("REBELION")
@@ -13,12 +13,15 @@ radio.onReceivedString(function (receivedString) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        strip.showColor(neopixel.rgb(255, 0, 0))
-        strip.show()
+        while (true) {
+            strip.showColor(neopixel.rgb(255, 0, 0))
+            strip.showColor(neopixel.rgb(0, 255, 0))
+            strip.show()
+        }
     } else if (receivedString == "temperatura") {
         basic.showString("" + (input.temperature()))
     } else {
-        if (receivedString == "moverse") {
+        if (receivedString == "parar") {
             bucle = 0
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
