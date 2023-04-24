f965f6518d956416ac5b49ad3ba157474559550b
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-04-24 11:17:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,20 +10,33 @@ input.onButtonPressed(Button.A, function () {
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "moverse") {
-        Led = 1
+        Led = 2
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        while (Led == 2) {
+            strip.showColor(neopixel.rgb(255, 0, 0))
+            strip.show()
+        }
     } else if (receivedString == "temperatura") {
         basic.showString("" + (input.temperature()))
         Led = 1
+        while (Led == 1) {
+            strip.showColor(neopixel.rgb(255, 0, 0))
+            strip.show()
+            strip.showColor(neopixel.rgb(0, 255, 0))
+            strip.show()
+        }
     } else {
         if (receivedString == "parar") {
             Led = 0
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
             maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            while (Led == 0) {
+                strip.clear()
+            }
         }
     }
 })
@@ -33,14 +46,3 @@ strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 radio.setGroup(12)
 let color = 0
 let bucle = 0
-basic.forever(function () {
-    while (Led == 1) {
-        strip.showColor(neopixel.rgb(255, 0, 0))
-        strip.show()
-        strip.showColor(neopixel.rgb(0, 255, 0))
-        strip.show()
-    }
-    while (Led == 0) {
-        strip.clear()
-    }
-})
