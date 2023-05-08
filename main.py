333e57a2b62137915fb760501414c9f380cbf420
Pggt123 <127737193+Pggt123@users.noreply.github.com>
2023-05-08 10:50:42 +0200

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def on_button_pressed_a():
-    radio.send_string("Adios")
-    strip.show_color(neopixel.rgb(0, 255, 0))
+    radio.send_string("parar")
+    strip.clear()
+    strip.show_color(neopixel.rgb(255, 200, 0))
     strip.show()
     maqueen.motor_stop(maqueen.Motors.ALL)
     basic.show_string("REBELION")
@@ -9,29 +10,37 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_string(receivedString):
-    global bucle
-    if receivedString == "Hola":
+    global Led
+    if receivedString == "moverse":
+        Led = 2
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
         maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
-        strip.show_color(neopixel.rgb(255, 0, 0))
-        strip.show()
-    elif receivedString == "Temperatura":
+        while Led == 2:
+            strip.show_color(neopixel.rgb(255, 0, 0))
+            strip.show()
+    elif receivedString == "temperatura":
         basic.show_string("" + str((input.temperature())))
+        Led = 1
+        while Led == 1:
+            strip.show_color(neopixel.rgb(255, 0, 0))
+            strip.show()
+            strip.show_color(neopixel.rgb(0, 255, 0))
+            strip.show()
     else:
-        if receivedString == "Adios":
-            bucle = 0
+        if receivedString == "parar":
+            Led = 0
             maqueen.motor_stop(maqueen.Motors.ALL)
             maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
             maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
-            strip.show_color(neopixel.rgb(0, 0, 0))
-            strip.show()
+            while Led == 0:
+                strip.clear()
 radio.on_received_string(on_received_string)
 
-bucle = 0
+Led = 0
 strip: neopixel.Strip = None
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-radio.set_group(35)
+radio.set_group(12)
 color = 0
 bucle = 0
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("parar")
     strip.clear()
@@ -20,7 +26,7 @@ radio.onReceivedString(function (receivedString) {
             strip.show()
         }
     } else if (receivedString == "temperatura") {
-        basic.showString("" + (input.temperature()))
+        basic.showString("" + input.temperature())
         Led = 1
         while (Led == 1) {
             strip.showColor(neopixel.rgb(255, 0, 0))
@@ -28,21 +34,19 @@ radio.onReceivedString(function (receivedString) {
             strip.showColor(neopixel.rgb(0, 255, 0))
             strip.show()
         }
-    } else {
-        if (receivedString == "parar") {
-            Led = 0
-            maqueen.motorStop(maqueen.Motors.All)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-            while (Led == 0) {
-                strip.clear()
-            }
+    } else if (receivedString == "parar") {
+        Led = 0
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        while (Led == 0) {
+            strip.clear()
         }
     }
 })
 let Led = 0
 let strip: neopixel.Strip = null
+let bucle = 0
+let color = 0
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 radio.setGroup(12)
-let color = 0
-let bucle = 0
